f306f85b0731e0a637516c6becbc05fd9acf968d
Julien Goletto <julien.goletto@gmail.com>
2023-06-22 18:00:35 +0200

    :sparkles: Indiv article pages are fixed

--- app/articles/[slug]/page.tsx
+++ app/articles/[slug]/page.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import dynamic from 'next/dynamic';
-
-// const getMDXArticleComponent = (slug: string) => {
-//   const folder = 'articles/';
-//   const articlePath = `${folder}${slug}.mdx`;
-//   const ArticleComponent = dynamic(import(`@/${articlePath}`));
-//   return ArticleComponent;
-// };
+import { getArticleDataBySlug } from '@/utils';
 
 const ArticlePage = async ({params}: {params: {slug: string}}) => {
-  const articlePath = `articles/modes-rendus-frontend.mdx`;
-  // const MDXArticleComponent = dynamic(async () => import(`@/articles/modes-rendus-frontend.mdx`));
-  // const MDXArticleComponent = dynamic(async () => import(`articles/modes-rendus-frontend.mdx`));
-  const MDXArticleComponent = dynamic(async () => import(`${articlePath}`));
-  // const MDXArticleComponent = getMDXArticleComponent(params.slug);
+  const {meta, content} = await getArticleDataBySlug(params.slug);
   return(
     <section>
       <h1>{params.slug}</h1>
       <article>
-          <MDXArticleComponent/>
+          {content}
       </article>
     </section>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,9 @@ import { getArticleSlugList, getArticleMetaBySlug } from '@/utils';
 export default async function Home() {
   const articlesSlugList = await getArticleSlugList();
   const articlesListWithMeta = await Promise.all(articlesSlugList.map(async(slug) => (getArticleMetaBySlug(slug))));
-  const articleCards = articlesListWithMeta.map(({title}) => (
+  const articleCards = articlesListWithMeta.map(({title, slug}) => (
     <div key={title}>
-      <Link href={`/articles/${title}`}>
+      <Link href={`/articles/${slug}`}>
         <h2>{title}</h2>
       </Link>
     </div>
