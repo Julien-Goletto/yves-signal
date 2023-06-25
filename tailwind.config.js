35f14f346f10836e4cde1a9bbb5a531ae02519fd
Julien Goletto <julien.goletto@gmail.com>
2023-06-25 15:29:43 +0200

    :lipstick: Adding custom colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './utils/**/*.{js,ts,jsx,tsx,mdx}',
+    // './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    // './components/**/*.{js,ts,jsx,tsx,mdx}',
+    // './utils/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
@@ -13,6 +13,25 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      theme: {
+        colors: {
+          'primary-dark': '#062347',
+          'primary-regular': '#0F478A',
+          'primary-medium': '#2677D9',
+          'primary-light': '#85AEE0',
+          'secondary-dark': '#350137',
+          'secondary-regular': '#840F8A',
+          'secondary-medium': '#D126D9',
+          'secondary-light': '#DC85E0',
+          'white': '#F1F2F4',
+          'gray': '#C4CBD4',
+          'black': '#011936',
+          'movies': '#FC9A3F',
+          'videogames': '#6AE692',
+          'books': '#FDFD96',
+          'series': '#FF6961',
+        },
+      }
     },
   },
   plugins: [],
