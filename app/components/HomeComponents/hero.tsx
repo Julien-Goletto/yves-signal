d3fc9ea5ce828601f08579dbd010bd1fa9df6fe3
Julien Goletto <julien.goletto@gmail.com>
2023-06-26 17:23:42 +0200

    :lipstick: Responsive, done

--- app/components/HomeComponents/hero.tsx
+++ app/components/HomeComponents/hero.tsx
@@ -6,9 +6,9 @@ import { AnimatedBlob } from '@components/Backgrounds/animated-blob';
 
 export const HeroSection = () => {
   return (
-    <section id="hero" className='relative w-full flex flex-col gap-8'>
-      <div className='z-10 custom-container'>
-        <div className='flex flex-col gap-4'>
+    <section id="hero" className='relative w-full flex flex-col gap-8 py-4 md:py-8 lg:py-12 xl:py-16'>
+      <div className='z-10 custom-container lg:flex-row'>
+        <div className='w-full flex flex-col gap-4 md:flex-row md:items-center'>
           <Image 
             src="/assets/avatar_hello.png" 
             alt="Yves Signal' avatar" 
@@ -16,12 +16,13 @@ export const HeroSection = () => {
             height={200}
             className='w-[100px] h-[100px] md:w-[200px] md:h-[200px]' />
           <h1>
-              <span className='text-3xl'>{"Bonjour, je suis \n"}</span>
+              <span className='text-3xl md:text-4xl'>{"Bonjour, je suis \n"}</span>
               <br/>
-              <span className='bg-clip-text text-transparent bg-gradient-to-r from-primary-medium to-secondary-medium'>Yves Signal</span>.</h1>
+              <span className='bg-clip-text text-transparent bg-gradient-to-r from-primary-medium to-secondary-medium'>Yves Signal</span>.
+          </h1>
         </div>
-        <div className='flex flex-col gap-8'>
-          <div className='flex flex-col gap-4 text-3xl'>
+        <div className='w-full flex flex-col gap-8'>
+          <div className='flex flex-col items-start gap-4 text-3xl'>
             <p>Bienvenue dans mon univers 🚀</p>
             <Button to="/presentation">Faire connaissance</Button>
           </div>
@@ -32,7 +33,7 @@ export const HeroSection = () => {
           </p>
         </div>
       </div>
-      <div className='absolute inset-0 -left-[50%]'>
+      <div className='absolute inset-0 -left-[50%] md:-left-[0%] lg:left-[15%] lg:-top-[30%] 2xl:left-[30%] 2xl:-top-[20%]'>
         <AnimatedBlob />
       </div>
     </section>
